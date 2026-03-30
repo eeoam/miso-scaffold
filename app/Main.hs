3db48689978a31fac80ab547808c7033556e79e2
Eric Macaulay <emmanuel@whetztone.net>
2026-03-30 11:31:00 +0100

    First commit

---
+++ app/Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE CPP #-}
+
+module Main where
+
+import Miso
+import Miso.Lens
+import Miso.String
+import Miso.Html.Element as H 
+import Miso.Html.Event as E 
+import Miso.Html.Property as P 
+import Miso.CSS qualified as CSS 
+
+#ifdef WASM
+#ifndef INTERACTIVE
+foreign export javascript "hs_start" main :: IO ()
+#endif
+#endif
+
+main :: IO ()
+#ifdef INTERACTIVE
+main = reload defaultEvents app
+#else 
+main = startApp defaultEvents app 
+#endif
+
+type State = ()
+
+data Action
+  = Skip
+  deriving (Show, Eq)
+
+app :: App State Action
+app = (component () updateModel viewModel)
+      { styles = []
+      }
+
+updateModel :: Action -> Effect parent State Action
+updateModel = noop
+
+viewModel :: State -> View () Action
+viewModel state = text "Hello!"
+
+sheet :: CSS.Stylesheet
+sheet = CSS.sheet_ []
